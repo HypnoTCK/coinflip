3527674169b53215b05193f5f49d33db9267eede
Vinicius Matos <viniciusmatos2018@outlook.com>
2024-10-29 23:53:37 -0300

    !wheel command is now dynamic (poggers)

---
+++ script.js
@@ -0,0 +1,9 @@
+function isInt(num) {
+    if (typeof num === 'number') {
+      return num - num === 0;
+    }
+    if (typeof num === 'string' && num.trim() !== '') {
+      return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+    }
+    return false;
+  };
